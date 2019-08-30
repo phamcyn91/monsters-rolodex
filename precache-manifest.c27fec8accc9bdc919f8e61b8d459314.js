96067fb70b5a50443305b9d330eaf5d680fd5e73
Cynthia Pham <cpham15@mail.greenriver.edu>
2019-08-30 15:20:54 -0700

    Updates

---
+++ precache-manifest.c27fec8accc9bdc919f8e61b8d459314.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf13df4b77edf7615d8df6df2fcd571d",
+    "url": "/phamcyn91/MonsterLibrary.git/index.html"
+  },
+  {
+    "revision": "f42d3713c79206b34320",
+    "url": "/phamcyn91/MonsterLibrary.git/static/css/main.e8f4a21d.chunk.css"
+  },
+  {
+    "revision": "ebdf587097504289c06e",
+    "url": "/phamcyn91/MonsterLibrary.git/static/js/2.3adefb50.chunk.js"
+  },
+  {
+    "revision": "f42d3713c79206b34320",
+    "url": "/phamcyn91/MonsterLibrary.git/static/js/main.2c859d77.chunk.js"
+  },
+  {
+    "revision": "7853da3c46d5158e803e",
+    "url": "/phamcyn91/MonsterLibrary.git/static/js/runtime~main.89317c64.js"
+  }
+]);
